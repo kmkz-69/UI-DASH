9d7a0f0eaf8a4dc71f43115cf60b0aa8e1f942c9
Jason Michel YAWILHIT <67412667+kmkz-69@users.noreply.github.com>
2020-07-21 17:18:09 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UI_DASH.ViewModel;
+
+namespace UI_DASH
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        private void btnUpgrade_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
